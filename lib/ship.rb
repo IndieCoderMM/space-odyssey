4d6b64fa2cacd06ff5cc15cadf52cef9683351d1
heinthant <hthant00chk@gmail.com>
2023-03-13 00:04:08 +0630

    Format code

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,50 +1,51 @@
-require_relative "./utils"
+require_relative './utils'
 
-IMG_PATH = 'assets/ship_green.png'
+IMG_PATH = 'assets/ship_green.png'.freeze
 
-class Ship 
-    attr_accessor :v_x, :v_y, :angle, :moving, :img
+class Ship
+  attr_accessor :v_x, :v_y, :angle, :moving, :img
 
-    def initialize(x, y)
-        @x = x
-        @y = y
-        @angle = 0
-        @speed = 5
-        @v_x = 0
-        @v_y = 0
-        @width = 100
-        @height = 80
-        @moving = false
-        @rect = {x: @x, y: @y, width: @width, height: @height}
-        @img = nil
-    end 
+  def initialize(x, y)
+    @x = x
+    @y = y
+    @angle = 0
+    @speed = 5
+    @v_x = 0
+    @v_y = 0
+    @width = 100
+    @height = 80
+    @moving = false
+    @rect = { x: @x, y: @y, width: @width, height: @height }
+    @img = nil
+  end
 
-    def draw
-			@img = Image.new(IMG_PATH, x: @x, y: @y, rotate: @angle)
+  def draw
+    @img = Image.new(IMG_PATH, x: @x, y: @y, rotate: @angle)
+  end
+
+  def update
+    return unless moving
+
+    direction = @angle * Math::PI / 180
+
+    @x += @speed * Math.sin(direction)
+    @y -= @speed * Math.cos(direction)
+
+    @rect[:x] = @x
+    @rect[:y] = @y
+  end
+
+  def jump
+    rand_x = rand($window_width)
+    rand_y = rand($window_height)
+    @x = rand_x
+    @y = rand_y
+  end
+
+  def is_collision?(asteroids)
+    asteroids.each do |asteroid|
+      return true if rect_collide?(@rect, asteroid.rect)
     end
-
-    def update 
-			return if !moving
-			direction = @angle * Math::PI / 180
-
-			@x += @speed * Math.sin(direction)
-			@y -= @speed * Math.cos(direction)
-
-			@rect[:x] = @x 
-			@rect[:y] = @y 
-		end 
-
-		def jump 
-			rand_x = rand($window_width)
-			rand_y = rand($window_height)
-			@x = rand_x
-			@y = rand_y
-		end
-
-    def is_collision?(aestroids)
-      aestroids.each do |aestroid|
-        return true if rect_collide?(@rect, aestroid.rect)
-      end 
-      return false 
-    end
+    false
+  end
 end
--- main.rb
+++ main.rb
@@ -1,47 +1,45 @@
 require 'ruby2d'
-require_relative "./lib/ship"
-require_relative "./lib/aestroid"
+require_relative './lib/ship'
+require_relative './lib/asteroid'
 
 $window_width = 640
 $window_height = 480
 
-set title: "Space Odyssey🚀", background: "navy"
+set title: 'Space Odyssey🚀', background: 'navy'
 set width: $window_width, height: $window_height
 
 ship = Ship.new(100, 50)
-aestroids = Array.new(5) {Aestroid.new}
+asteroids = Array.new(5) { Asteroid.new }
 
-update do 
-    clear
-    ship.update 
-    ship.draw 
-		aestroids.each do |aestroid|
-			aestroid.draw 
-			aestroid.update
-		end
-    if ship.is_collision?(aestroids)
-      ship.img.color = "red"
-    end
+update do
+  clear
+  ship.update
+  ship.draw
+  asteroids.each do |asteroid|
+    asteroid.draw
+    asteroid.update
+  end
+  ship.img.color = 'red' if ship.is_collision?(asteroids)
 end
 
 on :key_held do |event|
-	case event.key 
-	when "right"
-		ship.angle += 3
-	when "left"
-		ship.angle -= 3
-	when "up" 
-		ship.moving = true
-	end
+  case event.key
+  when 'right'
+    ship.angle += 3
+  when 'left'
+    ship.angle -= 3
+  when 'up'
+    ship.moving = true
+  end
 end
 
 on :key_up do |event|
-	case event.key 
-	when "up"
-		ship.moving = false
-	when "down"
-		ship.jump
-	end
+  case event.key
+  when 'up'
+    ship.moving = false
+  when 'down'
+    ship.jump
+  end
 end
 
 show
